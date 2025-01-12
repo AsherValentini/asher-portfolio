cf3be95e10e88d267e9a7cf99d916cacdfd52c3a
AsherValentni <ashermvalentini@gmail.com>
2025-01-12 23:15:39 +0100

    new images for app dev

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ console.log("Your personal website loaded with JetBrains style! :)");
 
 // Hover-activated video playback
 const videos = document.querySelectorAll('.portfolio-video');
+const bst_videos = document.querySelectorAll('.portfolio-video-bst');
 
 videos.forEach(video => {
+  video.addEventListener('mouseenter', () => {
+    video.play();
+  });
+  video.addEventListener('mouseleave', () => {
+    video.pause();
+  });
+});
+
+bst_videos.forEach(video => {
   video.addEventListener('mouseenter', () => {
     video.play();
   });
